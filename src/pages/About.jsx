0f28dfe9d0024fc7f85af6e2ffce42108d7319e5
neeljoe <joe.neel@gmail.com>
2025-04-11 16:47:12 +0530

    imagekit

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from '../components/Image'
+
+const About = () => {
+  return (
+    <div>About
+
+      <Image src='FCR/teampics/jersy2.jpg' className='rounded-3xl' />
+    </div>
+  )
+}
+
+export default About
