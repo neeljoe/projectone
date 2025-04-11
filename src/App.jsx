c136ab670b62c405de5a358dcc74aadf23825c85
neeljoe <joe.neel@gmail.com>
2025-04-11 15:05:05 +0530

    router for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter,
+          Routes,
+          Route
+ } from 'react-router'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div >App
-      
+    <div >
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
